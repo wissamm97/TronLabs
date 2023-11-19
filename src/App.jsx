f8708ea62bbbc174c60b4dfb9e70e29749cf62e7
Hannan Zubair <ahannanzubair@gmail.com>
2023-11-18 22:00:04 -0500

    committing all files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import tronLabsLogo from "./assets/tronLabsLogo.png";
+
+function App() {
+  return (
+    <>
+      <img src={tronLabsLogo} />
+      <h1>Advancing the world with intelligent technology.</h1>
+    </>
+  );
+}
+
+export default App;
